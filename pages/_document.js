36e4e33a0a8c637c984bd6a1e8accf38f1ff9cf9
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2023-02-01 18:06:12 +0530

    Changed Theme

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
-        <meta name="theme-color" content="#fff" />
+        <meta name="theme-color" content="#000000" />
       </Head>
       <body>
         <Main />
